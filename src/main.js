45680ef8f07da5e5ff3e172c871d214652f0023b
arjunsingla <arjun0999@gmail.com>
2024-04-02 17:56:42 -0700

    changed secret word for 2 year anniversary

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const secretMessage = "happy birthday himani".toUpperCase();
+const secretMessage = "years".toUpperCase();
 const NUMBER_OF_GUESSES = 6;
 
 const secretWord = secretMessage.replaceAll(" ", ""); // the secret message without spaces
