dde372953ae38ffeef142e5786d8e6e1ac66cbc1
arjunsingla <arjun0999@gmail.com>
2024-04-18 13:52:14 -0700

    new word as a surprise

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const secretMessage = "tulip".toUpperCase();
+const secretMessage = "awcute".toUpperCase();
 const NUMBER_OF_GUESSES = 6;
 
 const secretWord = secretMessage.replaceAll(" ", ""); // the secret message without spaces
