2bf11811b1bb5b512eb5a23dbeeda1929556325e
arjunsingla <arjun0999@gmail.com>
2024-06-19 12:33:59 -0400

    big apple babygit add .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const secretMessage = "drink".toUpperCase();
+const secretMessage = "apple".toUpperCase();
 const NUMBER_OF_GUESSES = 6;
 
 const secretWord = secretMessage.replaceAll(" ", ""); // the secret message without spaces
