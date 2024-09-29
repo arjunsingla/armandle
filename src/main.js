9a291d1197d150320220b34f50697ac1b5ed3bc6
arjunsingla <arjun0999@gmail.com>
2024-09-28 19:07:44 -0700

    wine bar date

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const secretMessage = "legos".toUpperCase();
+const secretMessage = "swirl".toUpperCase();
 const NUMBER_OF_GUESSES = 6;
 
 const secretWord = secretMessage.replaceAll(" ", ""); // the secret message without spaces
